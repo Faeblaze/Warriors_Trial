a83d63b3714a6d0f20528082d0aa45fb2c0c94fe
Zalron <davidrlawson@optusnet.com.au>
2018-07-23 16:36:37 +1000

    Fixed ainim to attacks.....
    YAY nick OPOP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 using System;
 
 public class Player : MonoBehaviour
 {
-  /*  Animator animator;//You may not need an animator, but if so declare it here   
+    private Animator animator;//You may not need an animator, but if so declare it here 
 
     int noOfClicks; //Determines Which Animation Will Play
     bool canClick; //Locks ability to click during animation event
@@ -33,7 +34,7 @@ public class Player : MonoBehaviour
 
         if (noOfClicks == 1)
         {
-            animator.SetInteger("slash1 1", 1);
+            animator.SetTrigger("swing");
         }
     }
 
@@ -42,37 +43,37 @@ public class Player : MonoBehaviour
 
         canClick = false;
 
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("1HAttack") && noOfClicks == 1)
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("swing") && noOfClicks == 1)
         {//If the first animation is still playing and only 1 click has happened, return to idle
-            animator.SetInteger("slash1 1", 4);
+            animator.SetTrigger("Attack");
             canClick = true;
             noOfClicks = 0;
         }
-        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("1HAttack") && noOfClicks >= 2)
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("swing") && noOfClicks >= 2)
         {//If the first animation is still playing and at least 2 clicks have happened, continue the combo          
-            animator.SetInteger("slash1 1", 2);
+            animator.SetTrigger("Attack");
             canClick = true;
         }
-        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("1HAttack 0") && noOfClicks == 2)
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("1HAttack") && noOfClicks == 2)
         {  //If the second animation is still playing and only 2 clicks have happened, return to idle         
-            animator.SetInteger("slash1 1", 4);
+            animator.SetTrigger("Attack");
             canClick = true;
             noOfClicks = 0;
         }
-        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("1HAttack 0") && noOfClicks >= 3)
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("1HAttack") && noOfClicks >= 3)
         {  //If the second animation is still playing and at least 3 clicks have happened, continue the combo         
-            animator.SetInteger("slash1 1", 3);
+            animator.SetTrigger("Attack");
             canClick = true;
         }
-        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("2HAttack3"))
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("2HAttack"))
         { //Since this is the third and last animation, return to idle          
-            animator.SetInteger("slash1 1", 4);
+            animator.SetTrigger("Attack");
             canClick = true;
             noOfClicks = 0;
         }
     }
 
-*/
+
 
 
 }
